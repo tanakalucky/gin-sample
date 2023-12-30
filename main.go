68a16e6bb1a1b2612f5a04a3b8f2f375731a9cc4
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2023-12-30 22:59:33 +0000

    todo一覧取得処理実装

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
-    engine:= gin.Default()
-    engine.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{
-            "message": "hello world",
-        })
-    })
+var (
+	host     = os.Getenv("HOST")
+	username = os.Getenv("USERNAME")
+	password = os.Getenv("USERPASS")
+	database = os.Getenv("DATABASE")
+	dsn      = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Tokyo", host, username, password, database)
+)
 
-    engine.Run(":8000")
+type Todo struct {
+    ID        uint       `gorm:"primarykey" json:"id"`
+    CreatedAt time.Time  `json:"-"`
+    UpdatedAt time.Time  `json:"-"`
+    Contents  string `gorm:"not null" json:"contents"`
+}
+
+func main() {
+	engine := gin.Default()
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("fail to connect database.")
+	}
+
+	db.AutoMigrate(&Todo{})
+
+	engine.GET("/todo", func(c *gin.Context) {
+		var todos []Todo
+
+		db.Find(&todos)
+
+		c.JSON(http.StatusOK, gin.H{
+			"items": todos,
+		})
+	})
+
+	engine.Run(":8000")
 }
